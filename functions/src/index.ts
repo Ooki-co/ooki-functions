793d91b075028b9e7c3a6bc30d90a8458d7502e1
Guvanch Nurgeldiyev <guvanch@nurgeldiyev.com>
2021-03-07 14:51:15 +0200

    fix: drop body-parser use native express parser

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,6 @@ import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as dotenv from 'dotenv';
 import * as cors from 'cors';
-import * as bodyParser from 'body-parser';
 import * as helmet from 'helmet';
 import {resolve} from 'path';
 
@@ -21,8 +20,8 @@ const app = express();
 
 app.use(helmet());
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.get('/artists', Artist.getAll);
 
@@ -39,7 +38,8 @@ export const api = functions
       memory: '256MB',
       timeoutSeconds: 60,
     })
-    .region('europe-west2').https.onRequest(app);
+    .region('europe-west2')
+    .https.onRequest(app);
 
 export const getAlbumsOfArtist = getAlbumsOfArtistTrigger;
 export const getTracksOfAlbum = getTracksOfAlbumTrigger;
